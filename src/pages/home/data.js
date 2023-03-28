497cd752886c42e0b6953dd46fbe969efb0215e3
Greg Rich <grich@madwell.com>
2023-03-28 15:14:20 -0400

    Fix typo.

--- src/pages/home/data.js
+++ src/pages/home/data.js
@@ -1,6 +1,6 @@
 const data = {
   title: "<span>Hello,</span> I'm Greg.",
-  description: "<p>I'm an Brooklyn-based Front End Developer, currently working at <a target=\"_blank\" rel=\"nofollow\" href=\"https://madwell.com\">Madwell</a>.</p>",
+  description: "<p>I'm a Brooklyn-based software developer, currently working at <a target=\"_blank\" rel=\"nofollow\" href=\"https://madwell.com\">Madwell</a>.</p>",
   cta: {
     url: "/about",
     text: "About Me"
