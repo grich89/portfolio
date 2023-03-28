40e9410031074ea38a613f9e05e0e817fe68a0d9
Greg Rich <grich@madwell.com>
2023-03-28 14:52:06 -0400

    Update timeline copy.

--- src/pages/home/data.js
+++ src/pages/home/data.js
@@ -1,6 +1,6 @@
 const data = {
   title: "<span>Hello,</span> I'm Greg.",
-  description: "<p>I'm an NYC-based Front End Developer, currently working at <a target=\"_blank\" rel=\"nofollow\" href=\"https://madwell.com\">Madwell</a>.</p>",
+  description: "<p>I'm an Brooklyn-based Front End Developer, currently working at <a target=\"_blank\" rel=\"nofollow\" href=\"https://madwell.com\">Madwell</a>.</p>",
   cta: {
     url: "/about",
     text: "About Me"
--- src/pages/timeline/data.js
+++ src/pages/timeline/data.js
@@ -3,31 +3,35 @@ const data = {
   timeline: [
     {
       year: "1999",
-      milestone: "Spent too much time on AOL and created my first website"
+      milestone: "Built my first website and became addicted to the internet"
     },
     {
       year: "2005",
-      milestone: "Learned to create a WordPress theme"
+      milestone: "Created my first Wordpress theme"
     },
     {
       year: "2011",
-      milestone: "Graduated from <strong>Merrimack College</strong> with a BA in Communications"
+      milestone: "Graduated from <strong>Merrimack College</strong> with a BA in Communications (proof that I can communicate!)"
     },
     {
       year: "2014",
-      milestone: "Swapped career paths and became a Web Developer at <a href=\"https://brafton.com\" target=\"_blank\" rel=\"nofollow\">Brafton</a>"
+      milestone: "Got my first tech job as a Web Developer at <a href=\"https://brafton.com\" target=\"_blank\" rel=\"nofollow\">Brafton</a>"
     },
     {
       year: "2019",
-      milestone: "Hired as Front End Developer at <a href=\"https://wearegenuine.com\" target=\"_blank\" rel=\"nofollow\">Genuine</a>"
+      milestone: "Front End Developer at <a href=\"https://wearegenuine.com\" target=\"_blank\" rel=\"nofollow\">Genuine</a>"
     },
     {
       year: "2020",
-      milestone: "Moved to <strong>NYC!</strong>"
+      milestone: "Moved from Boston to <strong>NYC</strong>) to fulfill a longtime <em>Friends</em>-inspired dream"
     },
     {
       year: "2021",
-      milestone: "Hired as Front End Developer at <a href=\"https://madwell.com\" target=\"_blank\" rel=\"nofollow\">Madwell</a>"
+      milestone: "Developer at <a href=\"https://madwell.com\" target=\"_blank\" rel=\"nofollow\">Madwell</a>"
+    },
+    {
+      year: "2023",
+      milestone: "Manhattan -> Brooklyn"
     }
   ]
 }
